dfb44a46eaf90ae2921f788530a4e8bea90ffe9b
Dave Jachimiak <dave.jachimiak@gmail.com>
2014-08-07 19:52:43 -0400

    Use peerHangUp message, remove unexpectedPeerHangUp message

--- lib/vega_server/events/close.rb
+++ lib/vega_server/events/close.rb
@@ -1,12 +1,12 @@
 require 'vega_server/cleanable'
-require 'vega_server/outgoing_messages/unexpected_peer_hang_up'
+require 'vega_server/outgoing_messages/peer_hang_up'
 
 module VegaServer::Events
   class Close
     include VegaServer::Cleanable
 
     def outgoing_message_class
-      VegaServer::OutgoingMessages::UnexpectedPeerHangUp
+      VegaServer::OutgoingMessages::PeerHangUp
     end
 
     def self.handle(websocket, event)
